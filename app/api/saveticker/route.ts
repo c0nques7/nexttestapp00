6cbd350e974689759de9783d74f43b07aa6f39f6
c0nques7 <c0nques7@gmail.com>
2024-05-19 01:57:27 -0400

    Update route.ts
    fixing known errors

--- app/api/saveticker/route.ts
+++ app/api/saveticker/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ message: 'Ticker added', user: updatedUser }, { status: 201 });
   } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error instanceof PrismaClientKnownRequestError) {
       if (error.code === 'P2002') { // Unique constraint
         return NextResponse.json({ error: 'Ticker already exists' }, { status: 409 });
       }
